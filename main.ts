226bb607aead95be0976aa80dbbb26bc2ecdbc7c
Anton Lioznov <lavton@gmail.com>
2023-09-04 15:42:17 +0300

    remove template code

--- main.ts
+++ main.ts
@@ -14,14 +14,6 @@ export default class MoveTagToFolderPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new MoveTagToFolderSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			// console.log('click', evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 		this.addCommand({
 			id: 'move-files-with-tag-forward',
 			name: 'Forward move files with tags to dir',
@@ -57,22 +49,6 @@ export default class MoveTagToFolderPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
